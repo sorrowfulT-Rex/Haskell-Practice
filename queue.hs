15691b7b3f4cad7fdb8f35b20b86fb1c109400a4
Yitang Chen <yc4120@ic.ac.uk>
2021-01-09 14:55:45 +0800

    より良いキュー

--- queue.hs
+++ queue.hs
@@ -3,10 +3,10 @@ module Queue where
 import           Control.Monad.Trans.State
 import           Data.Maybe
 
-data Queue a = Queue [a] [a]
+data Queue a = Queue Int [a] Int [a]
 
 instance Show a => Show (Queue a) where
-  show (Queue [] [])
+  show (Queue _ [] _ [])
     = "Empty Queue"
   show q
     = "[..." ++ show (fromJust $ evalState peek q) ++ "]"
@@ -15,47 +15,51 @@ type QueueS a = State (Queue a)
 
 emptyQueue :: Queue a
 emptyQueue
-  = Queue [] []
+  = Queue 0 [] 0 []
 
 -- Initialise a queue
 makeQueue :: [a] -> Queue a
-makeQueue 
-  = Queue []
+makeQueue as
+  = Queue 0 [] (length as) as
 
 -- Check if a queue is empty
 empty :: Queue a -> Bool
-empty (Queue [] [])
+empty (Queue _ [] _ [])
   = True
 empty _ 
   = False
+
+-- Check if we need to reverse the inbox list and fill it to the outbox list
+check :: Queue a -> Queue a
+check q@(Queue inl ins ol os)
+  | inl <= ol = q
+  | otherwise = Queue 0 [] (inl + ol) (os ++ reverse ins)
 
 -- Add element to the front of the queue
 push :: a -> QueueS a ()
 push e 
   = state $ \q -> ((), push' e q)
   where
-    push' e (Queue ins os) 
-      = Queue (e : ins) os
+    push' e (Queue inl ins ol os) 
+      = check (Queue (inl + 1) (e : ins) ol os)
 
 -- Add element to the end of the queue
 pushEnd :: a -> QueueS a ()
 pushEnd e 
   = state $ \q -> ((), pushEnd' e q)
   where
-    pushEnd' e (Queue ins os) 
-      = Queue ins (e : os)
+    pushEnd' e (Queue inl ins ol os) 
+      = Queue inl ins (ol + 1) (e : os)
 
 -- Retrieve element from the end of the queue
 pop :: QueueS a (Maybe a)
 pop 
   = state $ \q -> pop' q
   where
-    pop' q@(Queue [] [])
+    pop' q@(Queue _ _ [])
       = (Nothing, q)
-    pop' (Queue ins [])
-      = pop' $ Queue [] (reverse ins)
-    pop' (Queue ins (o : os))
-      = (Just o, Queue ins os)
+    pop' (Queue inl ins ol (o : os))
+      = (Just o, check (Queue inl ins (ol - 1) os))
 
 -- Look at the ending element of the queue without changing the queue
 peek :: QueueS a (Maybe a)
@@ -69,5 +73,5 @@ peek = do
 
 -- Length of the queue
 len :: Queue a -> Int
-len (Queue ins os) 
-  = length ins + length os
+len (Queue inl _ ol _) 
+  = inl + ol

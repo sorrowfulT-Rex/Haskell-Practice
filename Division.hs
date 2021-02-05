5c48931f029e90e23486b5af302862f4d40d6a8f
Yitang Chen <yc4120@ic.ac.uk>
2021-02-05 19:56:11 +0800

    Add unsigned multiplication

--- Division.hs
+++ Division.hs
@@ -10,11 +10,11 @@ import           Data.Foldable
 sigBit :: Int
 sigBit = 63
 
-division :: Int -> Int -> (Int, Int)
+division :: Word -> Word -> (Word, Word)
 division dividend divisor = execState (do
   forM_ [0..sigBit] $ \_ -> do
     (dvd, rmd) <- get
-    let (rmd', dvd') = twoShift rmd dvd
+    let (rmd', dvd') = twoShiftL rmd dvd
     if testBit rmd sigBit
       then assign dvd' (rmd' + divisor)
       else assign dvd' (rmd' - divisor)
@@ -24,7 +24,24 @@ division dividend divisor = execState (do
     else return ()
   ) (dividend, 0)
   where
-    twoShift a b
+    twoShiftL a b
       = (shiftL a 1 + if testBit b sigBit then 1 else 0, shiftL b 1)
     assign dvd rmd 
       = put (dvd + if testBit rmd sigBit then 0 else 1, rmd)
+
+
+--------------------------------------------------------------------------------
+-- Unsigned Multiplication
+--------------------------------------------------------------------------------
+
+multiplication :: Word -> Word -> (Word, Word)
+multiplication multiplicand multiplier = execState (do
+  forM_ [0..sigBit] $ \_ -> do
+    (pdt, mpc) <- get
+    if testBit mpc 0
+      then put $ twoShiftR (pdt + multiplier) mpc
+      else put $ twoShiftR pdt mpc
+  ) (0, multiplicand)
+  where
+    twoShiftR a b
+      = (shiftR a 1, shiftR b 1 + if testBit a 0 then bit sigBit else 0)
